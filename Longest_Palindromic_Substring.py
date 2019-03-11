8c6d8db7dd815abc4cd6ec6c645b3fb63c934374
zhuyanlong <zhuyanlongcs@hotmail.com>
2019-03-11 14:23:10 +0800

    Add files via upload

--- Longest_Palindromic_Substring.py
+++ Longest_Palindromic_Substring.py
@@ -5,18 +5,20 @@ def longestPalindrome( s: str) -> str:
 		len1=expandAroundCenter(s,i,i)
 		len2=expandAroundCenter(s,i,i+1)
 		lens=max(len1,len2)
-		if (end-start) <lens:
-			start=int(i-(lens-1)/2)
+		if (end-start+1) <lens:
+			start=int(i-(lens/2)+1)
 			end=int(i+lens/2)
 
-	return s[start+1:end+1]
+	return s[start:end+1]
 
 
 def expandAroundCenter(s, left, right):
 	while left>=0 and right <len(s) and s[left]==s[right]:
 		left-=1
 		right+=1
-	return right-left-1
+	left+=1
+	right-=1
+	return right-left+1
 
 def main():
 	s="babad"
