2be9d97b773ddbce1d12aafe2005b196213b7fbf
zhuyanlong <zhuyanlongcs@hotmail.com>
2020-06-08 15:31:00 -0500

    Add files via upload

--- Subsets.py
+++ Subsets.py
@@ -4,22 +4,20 @@ class Solution:
         if len(nums)==0:
             return []
         stack=[[]]
+        res.append([])
         while stack:
             top=stack.pop()
-            if top not in res:
-                res.append(top.copy())
             for item in nums:
-                if item not in top:
-                    if len(top)==0 or item >top[-1]:
-                        tmp=top.copy()
-                        tmp.append(item)
-                        res.append(tmp)
-                        stack.append(tmp)
+                if len(top)==0 or item >top[-1]:
+                    tmp=top.copy()
+                    tmp.append(item)
+                    stack.append(tmp)
+            res.append(tmp)
         return res
 
 def main():
     s=Solution
-    nums=[1,3,2,4]
+    nums=[1,2,5,3,4]
     print(s.subsets(s,nums))
 
 main()
