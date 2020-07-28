f40fca92931a2af08b05d7c312cc14a699e1eae0
Yanlong Zhu <zhuyanlongcs@163.com>
2020-07-28 17:32:43 +0800

    Add files via upload

--- KthLargestElementinanArray.py
+++ KthLargestElementinanArray.py
@@ -1,8 +1,8 @@
 #基本上把时间复杂度保持在O(nlgn)就可以了，空间复杂度要低一些
-class Solution:
-    def findKthLargest(self, nums, k):
-        nums.sort()
-        return nums[-k]
+# class Solution:
+#     def findKthLargest(self, nums, k):
+#         nums.sort()
+#         return nums[-k]
 
 #采用堆排序的思想，可以原地排序，并且时间复杂度在O(nlgn)
 class Solution:
@@ -15,15 +15,14 @@ class Solution:
             while m>1 and nums[m]>nums[m//2]:
                 nums[m],nums[m//2]=nums[m//2],nums[m]
                 m=m//2
-        result=0
-        # print(nums)
-        # m=k
-        while n>1:
+        m=k
+        while m>0:
             nums[1],nums[n-1]=nums[n-1],nums[1]
             # print(nums[n-1])
             n-=1
             i=1
-            while 2*i<n-1:
+            #这里的n-1是最后一个元素的下标
+            while 2*i<=n-1:
                 j=2*i
                 if j<n-1:
                     if nums[j]<nums[j+1]:
@@ -32,16 +31,14 @@ class Solution:
                     break
                 nums[i],nums[j]=nums[j],nums[i]
                 i=j
-        if len(nums)>3:
-            if nums[1]>nums[2]:
-                nums[1],nums[2]=nums[2],nums[1]
+            m-=1
         # print(nums)
         return nums[-k]
-    
+
 def main():
     s=Solution()
-    nums=[3,2,3,1,2,4,5,5,6]
-    k=4
+    nums=[0,1,0,90,89,88,87,86,85,84,83,82,81]
+    k=3
     print(s.findKthLargest(nums,k))
 
 main()
